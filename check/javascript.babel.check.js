f4743002fe0ad9a2f88171bfbda97d897fd02c9d
khai96_ <hvksmr1996@gmail.com>
2018-02-08 16:49:02 +0700

    Build process should be kept separate

--- check/javascript.babel.check.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-const path = require('path')
-const main = require('./lib/test-spawn')
-
-main({
-  defaultExecutable: 'sh',
-  defaultSpawnArguments: [path.resolve(__dirname, '../sh/build.sh')],
-  envMiddleName: 'BABELJS'
-})

61e4e5879186b81ac94b4e188035bea994215dda
khai96_ <hvksmr1996@gmail.com>
2018-02-10 11:57:24 +0700

    Use custom scripts

--- sh/install-peers.js
+++ sh/install-peers.js
@@ -1,9 +1,21 @@
 #! /usr/bin/env node
+'use strict'
+
+const {env: {YARN}, exit} = require('process')
+const {spawnSync} = require('child_process')
 const {peerDependencies} = require('../package.json')
 
-console.info(
-  Object
-    .entries(peerDependencies)
-    .map(([name, ver]) => `${name}@${ver}`)
-    .join('\n')
+const args = Object
+  .entries(peerDependencies)
+  .map(([name, ver]) => `${name}@${ver}`)
+
+const exec = ([cmd, ...prefix]) =>
+  spawnSync(cmd, [...prefix, ...args], {stdio: 'inherit'})
+
+const {status} = exec(
+  YARN === 'true'
+    ? ['yarn', 'add', '--peer']
+    : ['npm', 'install', '--save-peer']
 )
+
+exit(status)

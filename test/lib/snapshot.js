b9af1efb82da109bb6051bd486a7193179c3eae4
khai96_ <hvksmr1996@gmail.com>
2018-02-13 10:44:01 +0700

    Improve snap function

--- test/lib/snapshot.js
+++ test/lib/snapshot.js
@@ -1,6 +1,29 @@
+import React from 'react'
 import renderer from 'react-test-renderer'
 
-export const snapshot = subject =>
-  expect(renderer.create(subject).toJSON()).toMatchSnapshot()
+export const snapshot = subject => {
+  if (typeof subject === 'function') {
+    const value = (() => {
+      try {
+        const res = subject()
+
+        return {
+          value: res,
+          typeof: typeof res,
+          constructor: res && res.constructor && res.constructor.name,
+          string: res && typeof res.toString === 'function' && String(res)
+        }
+      } catch (error) {
+        return {error}
+      }
+    })()
+
+    expect(JSON.stringify(value)).toMatchSnapshot()
+  } else if (React.isValidElement(subject)) {
+    expect(renderer.create(subject).toJSON()).toMatchSnapshot()
+  } else {
+    expect(JSON.stringify(subject)).toMatchSnapshot()
+  }
+}
 
 export default snapshot
